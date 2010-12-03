7ecac89037aa27194c74a161487083760ebf603b
Mathias Dalheimer <md@gonium.net>
2010-12-03 11:32:31 +0100

    added parser for stick response

---
+++ libplugwise/response.cpp
@@ -0,0 +1,37 @@
+#include "response.hpp"
+#include <boost/lexical_cast.hpp>
+
+#include <iostream>
+
+using namespace plugwise;
+
+Response::Response (
+    const std::string& line1, 
+    const std::string& line2
+    ) :
+  _line1(line1), _line2(line2) 
+{
+  parse_line1();
+}
+
+bool Response::is_ok() {
+  if (acknowledge_code == 0xc1)
+    return true;
+  else
+    return false;
+}
+
+void Response::parse_line1() {
+  //std::cout << "Parsing first response line: " << _line1 << std::endl;
+  if (_line1.length() != 4*4)
+    throw plugwise::DataFormatException(
+        "Expected 16 chars for response line 1, got " + _line1.length()); 
+  //std::cout << "parsing: " << _line1.substr(0,4)  << std::endl;
+  command_code =boost::lexical_cast<uint32_from_hex>(_line1.substr(0,4));
+  //std::cout << "parsing: " << _line1.substr(4,4)  << std::endl;
+  sequence_number =boost::lexical_cast<uint32_from_hex>(_line1.substr(4,4));
+  //std::cout << "parsing: " << _line1.substr(8,4)  << std::endl;
+  acknowledge_code =boost::lexical_cast<uint32_from_hex>(_line1.substr(8,4));
+  //std::cout << "parsing: " << _line1.substr(12,4)  << std::endl;
+  checksum =boost::lexical_cast<uint32_from_hex>(_line1.substr(12,4));
+}
--- libplugwise/response.hpp
+++ libplugwise/response.hpp
@@ -2,21 +2,45 @@
 #define LIBPLUGWISE_RESPONSE_HPP 1
 
 #include <common.hpp>
+#include <string>
+#include <iostream>
 
 namespace plugwise {
   class Response {
     public:
       typedef std::tr1::shared_ptr<Response> Ptr;
       Response () {};
+      Response (const std::string& line1, 
+                const std::string& line2); 
       virtual std::string str() = 0;
+      void parse_line1();
+      bool is_ok();
       virtual ~Response() {};
+
+    protected:
+      std::string _line1;
+      std::string _line2;
+      uint32_t command_code;
+      uint32_t sequence_number;
+      uint32_t acknowledge_code;
+      uint32_t checksum;
 
     private:
       Response (const Response& original);
       Response& operator= (const Response& rhs);
-      
   };
-  
+  // helper for creating numerical variables from strings. See:
+  // http://stackoverflow.com/questions/1070497/c-convert-hex-string-to-signed-integer
+  class uint32_from_hex   // For use with boost::lexical_cast
+  {
+    uint32_t value;
+    public:
+    operator uint32_t() const { return value; }
+    friend std::istream& operator>>( std::istream& in, uint32_from_hex& outValue )
+    {
+      return in >> std::hex >> outValue.value;
+    }
+  };
 };
 
 
