7b779051209b93113ae26adcb9d6052bc33ce477
Mathias Dalheimer <md@gonium.net>
2010-12-14 10:27:09 +0100

    added some missing gpl headers

--- libplugwise/powerinformationrequest.cpp
+++ libplugwise/powerinformationrequest.cpp
@@ -1,3 +1,24 @@
+/**
+ * This file is part of libplugwise.
+ *
+ * (c) Mathias Dalheimer <md@gonium.net>, 2010
+ *
+ * libplugwise is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libplugwise is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libplugwise. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "powerinformationrequest.hpp"
 
 using namespace plugwise;
