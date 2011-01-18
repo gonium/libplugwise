fab0ee72430408bc38adfaef999b55df163d7e0d
Mathias Dalheimer <md@gonium.net>
2011-01-18 14:13:08 +0100

    added readme, added some commandline parsing

--- src/pw-read.cpp
+++ src/pw-read.cpp
@@ -27,52 +27,63 @@
 
 int main(int argc,char** argv) {
 
-  std::cout << "Please start with " << argv[0] << 
-    " /dev/ttyS1 (for example)" << std::endl;
-
-  plugwise::Connection::Ptr con(new plugwise::Connection(argv[1]));
-  plugwise::RequestFactory::Ptr reqFactory(new plugwise::RequestFactory("000D6F00007293BD"));
-  plugwise::ResponseFactory::Ptr respFactory(new plugwise::ResponseFactory(con));
-
-  std::cout << "### Initializing stick" << std::endl;
-  plugwise::Request::Ptr si_req=reqFactory->getStickInitRequest();
-  si_req->send(con);
-  plugwise::Response::Ptr si_resp=respFactory->receive();
-  std::cout << "### " << si_resp->str() << std::endl;
-  if (si_resp->req_successful())
-    std::cout << "initialization successful." << std::endl << std::endl;
-  else {
-    std::cout << "failed to initialize stick" << std::endl;
-    return -1;
+  if (argc != 3) {
+    std::cout << "Please start with " << argv[0] << 
+      " <stick device> "
+      " <circle-id> " << std::endl;
+    std::cout << "For example: " << argv[0] 
+      << " /dev/ttyUSB0 000D6F00007293BD" << std::endl;
+    return -4;
   }
 
-  std::cout << "### Sending calibration request " << std::endl;
-  plugwise::Request::Ptr ca_req=reqFactory->getCalibrationRequest();
-  ca_req->send(con);
-  plugwise::CalibrationResponse::Ptr ca_resp=respFactory->receiveCalibrationResponse();
-  std::cout << "### " << ca_resp->str() << std::endl;
-  if (ca_resp->req_successful())
-    std::cout << "calibration successful." << std::endl << std::endl;
-  else {
-    std::cout << "failed to read calibration values from circle " << std::endl;
-    return -2;
-  }
+  try {
+    plugwise::Connection::Ptr con(new plugwise::Connection(argv[1]));
+    plugwise::RequestFactory::Ptr reqFactory(new plugwise::RequestFactory(argv[2]));
+    plugwise::ResponseFactory::Ptr respFactory(new plugwise::ResponseFactory(con));
 
-  std::cout << "### Sending power information request " << std::endl;
-  plugwise::Request::Ptr pi_req=reqFactory->getPowerInformationRequest();
-  pi_req->send(con);
-  plugwise::PowerInformationResponse::Ptr pi_resp=
-    respFactory->receivePowerInformationResponse();
-  std::cout << "### " << pi_resp->str() << std::endl;
-  if (pi_resp->req_successful())
-    std::cout << "initialization successful." << std::endl << std::endl;
-  else {
-    std::cout << "failed to read calibration values from circle " << std::endl;
-    return -3;
-  }
+    std::cout << "### Initializing stick" << std::endl;
+    plugwise::Request::Ptr si_req=reqFactory->getStickInitRequest();
+    si_req->send(con);
+    plugwise::Response::Ptr si_resp=respFactory->receive();
+    std::cout << "### " << si_resp->str() << std::endl;
+    if (si_resp->req_successful())
+      std::cout << "initialization successful." << std::endl << std::endl;
+    else {
+      std::cout << "failed to initialize stick" << std::endl;
+      return -1;
+    }
 
-  std::cout << "Converting to current power consumption..." << std::endl;
-  plugwise::PowerConverter::Ptr pc(new plugwise::PowerConverter(ca_resp));
-  double watt=pc->convertToWatt(pi_resp);
-  std::cout << "Current power usage: " << watt << " Watt" << std::endl;
+    std::cout << "### Sending calibration request " << std::endl;
+    plugwise::Request::Ptr ca_req=reqFactory->getCalibrationRequest();
+    ca_req->send(con);
+    plugwise::CalibrationResponse::Ptr ca_resp=respFactory->receiveCalibrationResponse();
+    std::cout << "### " << ca_resp->str() << std::endl;
+    if (ca_resp->req_successful())
+      std::cout << "calibration successful." << std::endl << std::endl;
+    else {
+      std::cout << "failed to read calibration values from circle " << std::endl;
+      return -2;
+    }
+
+    std::cout << "### Sending power information request " << std::endl;
+    plugwise::Request::Ptr pi_req=reqFactory->getPowerInformationRequest();
+    pi_req->send(con);
+    plugwise::PowerInformationResponse::Ptr pi_resp=
+      respFactory->receivePowerInformationResponse();
+    std::cout << "### " << pi_resp->str() << std::endl;
+    if (pi_resp->req_successful())
+      std::cout << "initialization successful." << std::endl << std::endl;
+    else {
+      std::cout << "failed to read calibration values from circle " << std::endl;
+      return -3;
+    }
+
+    std::cout << "Converting to current power consumption..." << std::endl;
+    plugwise::PowerConverter::Ptr pc(new plugwise::PowerConverter(ca_resp));
+    double watt=pc->convertToWatt(pi_resp);
+    std::cout << "Current power usage: " << watt << " Watt" << std::endl;
+
+  } catch (plugwise::GenericException ge) {
+    std::cout << "Aborting: " << ge.what() << std::endl;
+  }
 }
